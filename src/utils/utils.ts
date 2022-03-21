b371ebae1e1fe5c473844386aee8baca5af3178c
Mir Abid Hussain <aabidhussain@gmail.com>
2022-03-21 00:18:05 -0400

    fixed eslint issues in utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,7 @@
 export function format(first: string, middle: string, last: string): string {
-  return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
+  return (
+    (first || '') +
+    (typeof middle !== 'undefined' ? ` ${middle}` : '') +
+    (typeof last !== 'undefined' ? ` ${last}` : '')
+  );
 }
