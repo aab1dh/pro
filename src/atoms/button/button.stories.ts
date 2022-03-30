1356876b5a79edbf6f79558e6490a62f49755744
Mir Abid Hussain <aabidhussain@gmail.com>
2022-03-30 17:21:54 -0400

    Added proptypes linting

--- src/atoms/button/button.stories.ts
+++ src/atoms/button/button.stories.ts
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { action } from '@storybook/addon-actions';
 import readme from './readme.md';
 import ButtonDocs from './button.docs.mdx';
-import { withDesign } from 'storybook-addon-designs'
+import { withDesign } from 'storybook-addon-designs';
+import PropTypes from 'prop-types';
 export default {
   title: 'Atoms',
   decorators: [withDesign],
@@ -9,7 +13,7 @@ export default {
     markdown: <Text>readme,
     notes: <Text>readme,
     docs: {
-      page: <Text>ButtonDocs
+      page: <Text>ButtonDocs,
     },
     design: {
       type: 'figma',
@@ -20,16 +24,21 @@ export default {
     },
   },
   argTypes: {
-    label: { control: 'text' }
+    label: { control: 'text' },
   },
 };
 
-const Template = args => `<pro-button ${{ ...args }} ${(onclick = () => action('clicked', { "clearOnStoryChange": false })())}>${args.label}</pro-button>`;
-
+const Template = (args: typeof propTypes) =>
+  `<pro-button ${{ ...args }} ${(onclick = () => action('clicked', { clearOnStoryChange: false })())}>${args.label
+  }</pro-button>`;
 
 export const Button = Template.bind({});
 
 Button.args = {
   ...Button.args,
-  label: 'Text button'
+  label: 'Text button',
+};
+
+const propTypes = {
+  label: PropTypes.string,
 };
