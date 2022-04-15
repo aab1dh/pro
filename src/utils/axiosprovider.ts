f414d85939844d354e995086cbbae4d09b7debca
Mir Abid Hussain <aabidhussain@gmail.com>
2022-04-14 21:53:35 -0400

    Global timeout for fetch

--- src/utils/axiosprovider.ts
+++ src/utils/axiosprovider.ts
@@ -6,12 +6,17 @@ export interface Provider<T> {
 
 export class AxiosProvider implements Provider<any> {
   async provide() {
+    const controller = new AbortController();
+
+    // 5 second timeout:
+    const timeoutId = setTimeout(() => controller.abort(), 5000);
     /* eslint-disable @typescript-eslint/no-unsafe-member-access */
     /* eslint-disable @typescript-eslint/no-unsafe-return */
     const url = 'https://jsonplaceholder.typicode.com/posts';
     try {
-      const res = await fetch(url);
+      const res = await fetch(url, { signal: controller.signal });
       set('posts', await res.json());
+      clearTimeout(timeoutId);
       return get('posts');
     } catch (error) {
       console.error(error);
--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,9 +1,11 @@
 const { fetch: originalFetch } = window;
 export const fetch = (window.fetch = async (...args) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const [resource, config] = args;
+  const [resource, config, timeout = 8000] = args;
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), timeout);
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  const response = await originalFetch(resource, config);
+  const response = await originalFetch(resource, { ...config, signal: controller.signal });
 
   // // request interceptor starts
   // resource = 'https://jsonplaceholder.typicode.com/todos/2';
@@ -24,5 +26,7 @@ export const fetch = (window.fetch = async (...args) => {
   //     .then(data => ({ ...data, title: `Intercepted` }));
 
   // response.json = json;
+
+  clearTimeout(timeoutId);
   return response;
 });
