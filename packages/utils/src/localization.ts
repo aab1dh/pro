3bab0a0f6f4883c3e7c47592f8e3e1607cf8bc90
Mir Abid Hussain <aabidhussain@gmail.com>
2022-04-16 22:52:51 -0400

    Fixed storybook run

--- packages/utils/src/localization.ts
+++ packages/utils/src/localization.ts
@@ -3,8 +3,7 @@
 let translation;
 
 const loadTranslation = async (locale = 'en-us') => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  translation = (await fetch(`${location.pathname}assets/i18n/${locale}.json`).then(async res => res.json()));
+  translation = (await fetch(`assets/i18n/${locale}.json`).then(async res => res.json()));
 };
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
